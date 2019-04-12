546eeb514f4ed3246c381b6c15f2c2b67dff8551
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-04-12 08:31:24 +0700

    brain : Adding the Rel fact

--- brain/gram.cxx
+++ brain/gram.cxx
@@ -1,4 +1,9 @@
+
 #encoding "utf-8"
 #GRAMMAR_ROOT All
 
-All -> Noun interp (Organ.Name) ;
+//All -> Noun<gnc-agr[1]> interp(Organ.Name) Noun<gnc-agr[1]> interp (Organ.Char) ;
+Ent -> Adj<gnc-agr[1]>+ Noun<gnc-agr[1], rt> interp (Organ.Name) ;
+Ent -> Noun interp (Organ.Name) ;
+All -> Ent interp (Rel.Name1 from Organ.Name) Verb interp (Rel.Type) Ent interp (Rel.Name2 from Organ.Name)  ;
+
